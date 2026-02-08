8a5671f2acb16da9c81b3d9c1bd1c0228dabe6e3
abhilashvalappil <abhilashvalappil123@gmail.com>
2026-02-08 18:46:55 +0530

    modified app.tsx file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,39 @@
-
-import { Routes, Route } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { Routes } from 'react-router-dom'
 import './App.css'
 import './index.css'
 import UserRoutes from './routes/userRoutes'
- 
+import { useDispatch } from 'react-redux';
+import { setUser } from './redux/slices/authSlice';
+import API from './utils/axiosInstance';
 
 const App: React.FC = () => {
+  const dispatch = useDispatch();
+  const [isInitializing, setIsInitializing] = useState(true);
+
+  useEffect(() => {
+    const initAuth = async () => {
+      try {
+        const response = await API.get('/auth/me');
+        if (response.data.success) {
+          dispatch(setUser(response.data.user));
+        }
+      } catch (error) {
+        console.log('No active session');
+      } finally {
+        setIsInitializing(false);
+      }
+    };
+    initAuth();
+  }, [dispatch]);
+
+  if (isInitializing) {
+    return <div className="loading-screen">Loading...</div>; // Simple loading state
+  }
 
   return (
     <Routes>
-        {UserRoutes()}
+      {UserRoutes()}
     </Routes>
   )
 }
